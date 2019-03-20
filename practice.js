f0002febc8be04cac7b7c9c67453023f03cd9f35
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-03-20 14:29:35 -0600

    Add solution 1-3

--- practice.js
+++ practice.js
@@ -19,6 +19,10 @@ var arr = [10,20,30];
 
 //Code Here
 
+function first(arr){
+ return arr[0]
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -34,7 +38,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+function last(arr){
+  return arr[arr.length - 1]
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +55,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for(var i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 ////////// PROBLEM 4 //////////
 
