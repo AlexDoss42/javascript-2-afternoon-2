82c74f8916ded36726497ed7834a1a78a80210fa
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-03-20 16:19:34 -0600

    Add Solution 11

--- practice.js
+++ practice.js
@@ -278,7 +278,12 @@ for(var i = 0; i < num2; i++){
 
 //Code Here
 
-
+function longer(arr1, arr2){
+  if(arr1.length > arr2.length){
+    return arr1
+  } 
+  return arr2
+}
 
 /*
   As a continuation of the previous problem, write another function called 'both'.
@@ -290,7 +295,17 @@ for(var i = 0; i < num2; i++){
 
 //Code Here
 
-
+function both(arr1, arr2){
+  let newArray = []
+  for(var i = 0; i < arr1.length; i++){
+    for(var j = 0; j < arr2.length; j++){
+      if(arr1[i] === arr2[j]){
+        newArray.push(arr2[j])
+      }
+    }
+  }
+  return newArray
+}
 
 ////////// PROBLEM 12 //////////
 
